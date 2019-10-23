6e0f59732a2bef8c07949a9feb89fc1cd413bcc9
Sehor97 <49066662+Sehor97@users.noreply.github.com>
2019-10-23 17:43:22 -0500

    Initial commit

---
+++ programa1.py
@@ -0,0 +1,181 @@
+class nodo:
+    def __init__(self):
+        self.i = None
+        self.j = None
+        self.value = None
+        self.revisado = ''
+
+
+class Matriz:
+
+    def __init__(self):
+        self.matriz = []
+        self.fila = 4
+        self.columna = self.fila
+        self.hijos = []
+        self.con = True
+
+    def poblar(self):
+        for i in range(int(self.fila)):
+            self.matriz.append([])
+            for j in range(int(self.columna)):
+                self.matriz[i].append(1)
+
+    def obstaculos(self):
+        self.matriz[1][2] = 0
+        self.matriz[2][2] = 0
+        self.matriz[2][1] = 0
+
+    def premio(self):
+        self.matriz[3][2] = 3
+
+    def imprimir(self):
+        imprimir = ""
+        for i in range(self.fila):
+            for j in range(self.fila):
+                imprimir += str("[")
+                imprimir += str(self.matriz[i][j])
+                imprimir += str("]")
+            imprimir += "\n"
+        print(imprimir)
+
+    def derecha(self, nodoent):
+        nodosal = None
+        if nodoent.j + 1 < self.fila:
+            if self.matriz[nodoent.i][nodoent.j + 1] != 0 and nodoent.revisado is not True:
+                nodosal = nodo()
+                itemd = self.matriz[nodoent.i][nodoent.j + 1]
+                nodosal.i = nodoent.i
+                nodosal.j = nodoent.j + 1
+                nodosal.value = itemd
+                nodosal.revisado = 'der'
+                print('----derecha-----')
+                print('---entrada')
+                print(nodoent.i, nodoent.j, nodoent.value, nodoent.revisado)
+                print('----salida----')
+                print(nodosal.i, nodosal.j, nodosal.value, nodosal.revisado)
+
+        return nodosal
+
+    def izquierda(self, nodoent):
+        nodosal = None
+        if nodoent.j - 1 >= 0:
+            if self.matriz[nodoent.i][nodoent.j - 1] != 0 and nodoent.revisado is not True:
+                nodosal = nodo()
+                itemi = self.matriz[nodoent.i][nodoent.j - 1]
+                nodosal.i = nodoent.i
+                nodosal.j = nodoent.j - 1
+                nodosal.value = itemi
+                nodosal.revisado = 'izq'
+                print('----izquierda-----')
+                print('---entrada')
+                print(nodoent.i, nodoent.j, nodoent.value, nodoent.revisado)
+                print('----salida----')
+                print(nodosal.i, nodosal.j, nodosal.value, nodosal.revisado)
+
+        return nodosal
+
+    def inferior(self, nodoent):
+        nodosal = None
+        if nodoent.i + 1 < self.columna:
+            if self.matriz[nodoent.i + 1][nodoent.j] != 0 and nodoent.revisado is not True:
+                nodosal = nodo()
+                items = self.matriz[nodoent.i + 1][nodoent.j]
+                nodosal.i = nodoent.i + 1
+                nodosal.j = nodoent.j
+                nodosal.value = items
+                nodosal.revisado = 'inf'
+                print('----inferior-----')
+                print('---entrada')
+                print(nodoent.i, nodoent.j, nodoent.value, nodoent.revisado)
+                print('----salida----')
+                print(nodosal.i, nodosal.j, nodosal.value, nodosal.revisado)
+
+        return nodosal
+
+    def superior(self, nodoent):
+        nodosal = None
+        if nodoent.i - 1 >= 0:
+            if self.matriz[nodoent.i - 1][nodoent.j] != 0 and nodoent.revisado is not True:
+                nodosal = nodo()
+                itemsup = self.matriz[nodoent.i - 1][nodoent.j]
+                nodosal.i = nodoent.i - 1
+                nodosal.j = nodoent.j
+                nodosal.value = itemsup
+                nodosal.revisado = 'sup'
+                print('----superior-----')
+                print('---entrada')
+                print(nodoent.i, nodoent.j, nodoent.value, nodoent.revisado)
+                print('----salida----')
+                print(nodosal.i, nodosal.j, nodosal.value, nodosal.revisado)
+
+        return nodosal
+
+    def find(self, nodo):
+        self.hijos.append(nodo)
+        continuar = self.con
+        colas = []
+        der = None
+        inf = None
+        sup = None
+        izq = None
+
+        if nodo.revisado == '':
+            inf = self.inferior(nodo)
+            der = self.derecha(nodo)
+            sup = self.superior(nodo)
+            izq = self.izquierda(nodo)
+        if nodo.revisado == 'izq':
+            izq = self.izquierda(nodo)
+            inf = self.inferior(nodo)
+            sup = self.superior(nodo)
+        if nodo.revisado == 'sup':
+            sup = self.superior(nodo)
+            der = self.derecha(nodo)
+            izq = self.izquierda(nodo)
+        if nodo.revisado == 'inf':
+            inf = self.inferior(nodo)
+            der = self.derecha(nodo)
+            izq = self.izquierda(nodo)
+        if nodo.revisado == 'der':
+            der = self.derecha(nodo)
+            inf = self.inferior(nodo)
+            sup = self.superior(nodo)
+
+        if inf is not None:
+            colas.append(inf)
+        if der is not None:
+            colas.append(der)
+        if izq is not None:
+            colas.append(izq)
+        if sup is not None:
+            colas.append(sup)
+
+        for i in range(len(colas)):
+            if colas[i].value == 3:
+                print(colas[i].value)
+                print('---fin---')
+                continuar = False
+                break
+
+        if continuar is True:
+            for i in range(len(colas)):
+                print(colas[i].value)
+                print('------')
+                self.find(colas[i])
+        else:
+            breakpoint()
+            print("ENCONTRADO")
+
+
+prueba = Matriz()
+prueba.poblar()
+prueba.obstaculos()
+prueba.premio()
+prueba.imprimir()
+inicial = nodo()
+inicial.i = 0
+inicial.j = 0
+inicial.value = prueba.matriz[0][0]
+inicial.revisado = ''
+prueba.find(inicial)
